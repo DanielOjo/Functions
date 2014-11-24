de48d353aa7fdd02708c72a1279ee46cbcc2b59f
DanielOjo <danielojo@live.com>
2014-11-24 12:16:44 +0000

    finished in class

--- Classroom starters/Classroom starters making calculate functions.py
+++ Classroom starters/Classroom starters making calculate functions.py
@@ -2,7 +2,7 @@
 #18-11-2014
 #Classroom starters making calculate functions
 
-#define a function
+#Define a function
 def calculate_basic_pay(hours,pay):
     basic_pay = hours * pay
     return basic_pay
@@ -22,11 +22,24 @@ def calculate_total_pay(hours,pay):
         total_pay = calculate_overtime_pay(hours,pay)
     return total_pay
 
-def work_details
+def work_details():
+    hours = int(input("Please enter the amount of hours that you worked this week: "))
+    pay = float(input("Please enter your pay rate: "))
+    return hours,pay
 
-def display_total_pay
+def display_total_pay(total_pay):
+    print("You will be paid {0}".format(total_pay))
+    
 
-def calculate_pay
+def calculate_pay():
+    hours,pay = work_details()
+    total_pay = calculate_total_pay(hours,pay)
+    display_total_pay(total_pay)
+
+#Main Program
+calculate_pay()
+    
+          
 
 
 
