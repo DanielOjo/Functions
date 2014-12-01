e4cc92b905bdceb0a807e12b49326787e2f6e273
DanielOjo <danielojo@live.com>
2014-12-01 12:16:36 +0000

    The coding could do with some work

--- Classroom exercises/functions_improvement_ex.py
+++ Classroom exercises/functions_improvement_ex.py
@@ -1,23 +1,10 @@
 #Daniel Ogunlana
 #Functions improvement exercise
 #Times-table tester
+
 import random
 
 #Functions
-
-def time_table():
-    testTable = input("Which times-table do you want to be tested on? ")
-    testTable = int(testTable)
-    return testTable
-
-def questions():
-    for questions in range(1,6):
-        Num1 = testTable
-        Num2 = random.randrange(2,13)
-        Ans = Num1 * Num2
-        UserAnswer = input(str(Num1) + ' x ' + str(Num2) + ' = ? ')
-        UserAnswer = int(UserAnswer)
-    return Num1,Num2,Ans,UserAnswer
 
 def user_true_or_false(UserAnswer,Ans):
     if UserAnswer == Ans:
@@ -26,22 +13,21 @@ def user_true_or_false(UserAnswer,Ans):
     else:
         print('Sorry, you got the answer wrong. The correct answer is',Ans)
         print()
+        
+def questions():
+    testTable = int(input("Which times-table do you want to be tested on? "))
+    for questions in range(1,6):
+        Num1 = testTable
+        Num2 = random.randrange(2,13)
+        Ans = Num1 * Num2
+        UserAnswer = input(str(Num1) + " x " + str(Num2) + " = ? ")
+        UserAnswer = int(UserAnswer)
+        user_true_or_false(UserAnswer,Ans)
 
-#Main program
+def test_table():
+    
+
+#main program
 print("Times-table tester")
 print()
-testTable = input("Which times-table do you want to be tested on? ")
-testTable = int(testTable)
-for questions in range(1,6):
-    Num1 = testTable
-    Num2 = random.randrange(2,13)
-    Ans = Num1 * Num2
-    UserAnswer = input(str(Num1) + ' x ' + str(Num2) + ' = ? ')
-    UserAnswer = int(UserAnswer)
-    if UserAnswer == Ans:
-        print('Well done, you got the correct answer!')
-        print()
-    else:
-        print('Sorry, you got the answer wrong. The correct answer is',Ans)
-        print()
-              
+questions()
--- Classroom exercises/test for task1.py
+++ Classroom exercises/test for task1.py
@@ -11,17 +11,17 @@ def user_true_or_false(UserAnswer,Ans):
         print()
         
 def questions():
+    testTable = input("Which times-table do you want to be tested on? ")
+    testTable = int(testTable)
     for questions in range(1,6):
         Num1 = testTable
         Num2 = random.randrange(2,13)
         Ans = Num1 * Num2
         UserAnswer = input(str(Num1) + ' x ' + str(Num2) + ' = ? ')
         UserAnswer = int(UserAnswer)
-        user_true_or_false
+        user_true_or_false(UserAnswer,Ans)
 
-def times_table_details():
-    testTable = input("Which times-table do you want to be tested on? ")
-    testTable = int(testTable)
+    
 
 
 
